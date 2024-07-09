aa7864e1fdc28b3081ed3c7b9deb2c0d3e3318bb
Nishanth1223 <nishanth6010@gmail.com>
2024-07-09 08:26:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# imports
+from streamlitapp import TrendsInDataJobs
+
+
+if __name__ == '__main__':
+    # Create an instance of the TrendsInDataJobs class
+    app = TrendsInDataJobs()
+    # Run the Streamlit application
+    app.run()
